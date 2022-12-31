631dcc1344fe7eebf2d21d931b0ea668317447db
Matheus <teteulopes2011@hotmail.com>
2022-12-31 11:53:55 -0300

    mudança de rota de inicio para /

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
 
 const routes: Routes = [
-  {path: 'inicio', component: InicioComponent}
+  {path: '', component: InicioComponent}
 ];
 
 @NgModule({
